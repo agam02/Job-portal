a2bdaa92df48f6c2cf8a51f2c4e8a21037db89f6
agam02 <97424357+agam02@users.noreply.github.com>
2024-02-27 14:54:54 +0530

    pushing files

---
+++ Client/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRsCkKUovWVkxl0R_r7rxfvD1vU2Ix9VA",
+  authDomain: "job-portal-87d46.firebaseapp.com",
+  projectId: "job-portal-87d46",
+  storageBucket: "job-portal-87d46.appspot.com",
+  messagingSenderId: "856756151291",
+  appId: "1:856756151291:web:81a956fbda1c3b2201a15e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
